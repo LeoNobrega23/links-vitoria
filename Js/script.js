378cccde5ad359d6480db0760f18417b8d340f76
Leonardo Nóbrega <leonardo.nobrega56@gmail.com>
2023-01-11 21:07:17 -0300

    del script

--- Js/script.js
+++ Js/script.js
@@ -1,31 +1,28 @@
 //Button
 
-// var btn = document.getElementById('btn')
-// var galery = document.getElementById('swiper')
-// btn.addEventListener('click', showGalery)
-// function showGalery() {
-//     if (swiper.style.display === 'none') {
-//         swiper.style.display = 'block'
-//     } else {
-//         swiper.style.display = 'none'
-//     }
-// };
+ var btn = document.getElementById('btn')
+ var galery = document.getElementById('swiper')
+ btn.addEventListener('click', showGalery)
+ function showGalery() {
+     if (swiper.style.display === 'none') {
+         swiper.style.display = 'block'
+     } else {
+         swiper.style.display = 'none'
+     }
+ };
 
 
 
 //Swiper
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 1,
-    spaceBetween: 19,
-  
-    // Navigation arrows
-    navigation: {
+ const swiper = new Swiper('.swiper', {
+     // Optional parameters
+     direction: 'horizontal',
+     loop: true,
+     slidesPerView: 1,
+     spaceBetween: 19,
+     // Navigation arrows
+     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  
-
-  });
+   });
