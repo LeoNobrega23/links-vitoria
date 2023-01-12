9bbc1bb1c8bf352e2c96859d8fc32a217fa9fbab
Leonardo Nóbrega <leonardo.nobrega56@gmail.com>
2023-01-11 21:08:47 -0300

    button delete

--- Js/script.js
+++ Js/script.js
@@ -1,17 +1,3 @@
-//Button
-
- var btn = document.getElementById('btn')
- var galery = document.getElementById('swiper')
- btn.addEventListener('click', showGalery)
- function showGalery() {
-     if (swiper.style.display === 'none') {
-         swiper.style.display = 'block'
-     } else {
-         swiper.style.display = 'none'
-     }
- };
-
-
 
 //Swiper
  const swiper = new Swiper('.swiper', {
